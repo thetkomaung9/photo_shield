e9237590761dfec60ee5b3c4dc9166b52baf39e6
thetkomaung <thetkomaung966@gmail.com>
2026-04-20 01:33:20 +0900

    Updated Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'app.dart';
-import 'core/services/push_notification_service.dart';
+// import 'core/services/push_notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Firebase 초기화
-  await Firebase.initializeApp();
-
-  // 백그라운드 메시지 핸들러 등록
-  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
-
-  // 푸시 알림 서비스 초기화
-  await PushNotificationService().initialize();
+  // TODO: Firebase 설정 후 활성화
+  // Firebase 초기화 - google-services.json 필요
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  // await PushNotificationService().initialize();
 
   runApp(const ProviderScope(child: PhotoShieldApp()));
 }
